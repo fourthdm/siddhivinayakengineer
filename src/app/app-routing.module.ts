a3c2de65b5d19bf4b7a89d39a76217d0c8876f9f
fourthdm <manisha.fourthdm@gmail.com>
2026-06-11 15:51:38 +0530

    hero section images chnage changes in contact page to make a field is required.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{useHash:false})],
+  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
